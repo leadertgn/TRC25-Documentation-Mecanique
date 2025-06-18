224d44b637d43c2d00b4e987968e06ec07a0aa3a
Emeric <tognonemeric@gmail.com>
2025-06-18 15:28:00 +0100

    Initial commit et Mise en place des fichiers principaux

---
+++ README.md
@@ -0,0 +1,1 @@
+# TRC25-Documentation-Mecanique
---
+++ js/loadNav.js
@@ -0,0 +1,15 @@
+function includeNav() {
+  fetch('/TRC25-Documentation-Mecanique/composants/nav.html')
+    .then(response => {
+      if (!response.ok) throw new Error("Navigation introuvable");
+      return response.text();
+    })
+    .then(data => {
+      document.getElementById('nav-placeholder').innerHTML = data;
+    })
+    .catch(error => {
+      console.error("Erreur de chargement de la nav :", error);
+    });
+}
+
+document.addEventListener('DOMContentLoaded', includeNav);
